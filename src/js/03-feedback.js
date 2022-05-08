aeca50ad5163b40bc9b537a69dc431e6aa99c7e0
Ann Sarviro <Annka_nuha@i.ua>
2022-05-08 20:12:12 +0300

    bugs task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,34 +2,39 @@
     import throttle from 'lodash.throttle';
 
     const formEl = document.querySelector('.feedback-form');
-    const inputEl = formEl.querySelector ('input');
-    const textEl = formEl.querySelector('textarea')
+    const inputEl = formEl.querySelector('input');
+    const textareaEl = formEl.querySelector('textarea');
     const LOCAL_KEY = "feedback-form-state"
     
-
-    const savedForm = localStorage.getItem(LOCAL_KEY);
-    const parsedForm = JSON.parse(savedForm);
-    
-    if (parsedForm) {
-        textEl.textContent = parsedForm.message;
-        inputEl.value = parsedForm.email;     
+const setForm = function() {
+        const savedForm = localStorage.getItem(LOCAL_KEY);
+        const parsedForm = JSON.parse(savedForm);
+        
+        
+        if (parsedForm) {
+            formEl.elements.email.value = parsedForm.email;
+            formEl.elements.message.value = parsedForm.message; 
+            }
     }
-    
+
 const clearMessage = function(event) {
     event.preventDefault();
     localStorage.clear();
-    console.log(userForm)
-    formEl.reset()
+    console.log(userForm);
+    formEl.reset();
 }
 
 const onFormSetLocal = function (event) {
-    userForm[event.target.name] = event.target.value;    
+    userForm[event.target.name] = event.target.value;   
     localStorage.setItem(LOCAL_KEY, JSON.stringify(userForm))
 }
 
-const userForm = {}
-
+let userForm = {};
 formEl.addEventListener('input', throttle(onFormSetLocal, 500));
 formEl.addEventListener("submit", clearMessage);
+setForm();
 
 
+
+
+   
