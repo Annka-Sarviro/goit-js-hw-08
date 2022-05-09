134587978c09938a92b7ee35e61cf6cc3d998064
Ann Sarviro <Annka_nuha@i.ua>
2022-05-09 16:21:21 +0300

    update task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,14 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox'
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 import createGalleryItem from './gallary/createGallery';
-import onOpenModalImg from './gallary/onOpenModalImg';
+
 
 const galleryBoxEl = document.querySelector('.gallery');
 const createGallaryMarkup = createGalleryItem(galleryItems);
 galleryBoxEl.insertAdjacentHTML('beforeend', createGallaryMarkup);
-
-galleryBoxEl.addEventListener('click', onOpenModalImg);
+let gallery = new SimpleLightbox('.gallery .gallery__item', {
+        
+    captionDelay: 250,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    
+});
 
