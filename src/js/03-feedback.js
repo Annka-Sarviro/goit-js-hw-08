8b7a1be20f3f7a13bbb5fa520b720456fbce409a
Ann Sarviro <Annka_nuha@i.ua>
2022-05-11 00:17:52 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,41 +2,45 @@
     import throttle from 'lodash.throttle';
 
     const formEl = document.querySelector('.feedback-form');
-    
+    const inputEl = formEl.querySelector('input');
+    const messageEl = formEl.querySelector('textarea');
     const LOCAL_KEY = "feedback-form-state"
-    
-const setForm = function() {
-       
-    const parsedForm = JSON.parse(localStorage.getItem(LOCAL_KEY));
-        
-        
- if (userForm) {
-            formEl.elements.email.value = parsedForm.email;
-            formEl.elements.message.value = parsedForm.message;
-            
-            }
-    }
-
-const clearMessage = function(event) {
-    event.preventDefault();
-    localStorage.clear();
-    console.log(userForm);
-    formEl.reset();
-}
-
-const onFormSetLocal = function (event) {    
-    userForm[event.target.name] = event.target.value;   
-    localStorage.setItem(LOCAL_KEY, JSON.stringify(userForm))
-    
-}
-
-let userForm = {};
-
-formEl.addEventListener('input', throttle(onFormSetLocal, 500));
-formEl.addEventListener("submit", clearMessage);
-setForm();
+    const userForm = {};
 
 
-
-
+    formEl.addEventListener('submit', onSubmitForm);
+    formEl.addEventListener('input', throttle(onInputForm, 500));
+    getSaveForm();
    
+
+
+
+    function onSubmitForm(event) {
+        event.preventDefault();
+        if (!userForm.email || !userForm.message) {
+            console.log('add you message or email')
+        } else  {
+            console.log(userForm)
+            formEl.reset();
+            localStorage.clear();
+                    
+             }
+    };
+
+    function onInputForm() {
+        userForm.email = inputEl.value;
+        userForm.message = messageEl.value
+
+        localStorage.setItem(LOCAL_KEY, JSON.stringify(userForm))
+    };
+
+    function getSaveForm() {
+        const parsedForm = JSON.parse(localStorage.getItem(LOCAL_KEY));
+
+        if (!parsedForm) {return}
+        if (parsedForm.email) {
+                    formEl.elements.email.value = parsedForm.email;}
+
+        if (parsedForm.message) {
+                    formEl.elements.message.value = parsedForm.message;}
+    };
